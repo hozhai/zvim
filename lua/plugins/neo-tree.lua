612c2cd4764dff35a3a2b2a6acd71893e8365261
Hongmeng Zhai <zhaihongmeng@gmail.com>
2024-09-22 22:10:49 -0300

    add neocodeium + mini.ai

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,24 +1,29 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	opts = {
-		filesystem = {
-			filtered_items = {
-				visible = true,
-				show_hidden_count = true,
-				hide_dotfiles = false,
-				hide_gitignored = false,
-				never_show = {},
-			},
-		},
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", "<Cmd>Neotree filesystem reveal right toggle<CR>", {})
-		vim.keymap.set("n", "<leader>bf", "<Cmd>Neotree buffers reveal float<CR>", {})
-	end,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    opts = {
+        close_if_last_window = false,
+        popup_border_style = "rounded",
+        -- enable_git_status = true,
+        -- enable_diagnostics = true,
+        -- open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                show_hidden_count = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+                never_show = {},
+            },
+        },
+    },
+    config = function()
+        vim.keymap.set("n", "<C-n>", "<Cmd>Neotree filesystem reveal right toggle<CR>", {})
+        vim.keymap.set("n", "<leader>bf", "<Cmd>Neotree buffers reveal float<CR>", {})
+    end,
 }

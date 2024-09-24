f431e81a3f53bb05e8639dc934bce00b66f9cbff
Hongmeng Zhai <hongmengzhai@zhai-macos.local>
2024-09-24 14:10:05 -0300

    added typewriter

---
+++ lua/plugins/typewriter.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        'joshuadanpeterson/typewriter',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+        },
+        config = function()
+            require('typewriter').setup()
+        end,
+        opts = {}
+    }
+}
